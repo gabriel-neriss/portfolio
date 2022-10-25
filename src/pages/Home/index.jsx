fd0db320789bdef594938aea479b30fb85349e7d
Gabriel Neris <gabriel.nerisg@gmail.com>
2022-10-25 11:34:30 -0300

    first commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import { Container } from "./styles"
+
+
+export function Home () {
+
+  return (
+    <Container>
+
+        <h1>Oi, eu sou o Gabriel, Desenvolvedor Full Stack</h1>
+
+    </Container>
+
+
+
+  )
+}
